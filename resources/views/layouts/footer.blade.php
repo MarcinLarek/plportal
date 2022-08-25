a5acc4d88d1106c5a53a8c009f8702b0b84efe85
MarcinLarek <marcinlarek00@gmail.com>
2022-08-25 17:08:52 +0200

    Przebudowa stopki i fix prawego panelu faktów

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,25 +1,19 @@
 <div class="bg-section border-top border-dark mt-5">
-    <div class="row">
-        <div class="col-4 pt-4" align="center">
-          <a href="#">Redakcja</a>  |
-          <a href="#">Regulamin</a>  |
-          <a href="#">O portalu</a>  |
-          <a href="#">Zgłoś naruszenie</a>  |
-          <a href="#">Wydawca</a>
-        </div>
-        <div class="col-4 pt-4" align="center">
-          ⓒ 2019 <a href="{{ route( 'index' ) }}"> <b> <u>PLPORTAL.pl</u> </b> </a> Wszelkie prawa zastrzeżone. </br>
-          Kontakt: <a href="mailto:redakcja@plportal.pl?subject=feedback"> <u>redakcja@plportal.pl</u> </a>
-        </div>
-        <div class="col-4 pt-4">
-          Developed by <a href="https://fundacjaglosmlodych.org/"> <u>Fundacja na Rzecz Promocji i Rozwoju „Głos Młodych”</u> </a>  and Marcin Larek
-        </div>
-    </div>
-    <div class="row" align="center">
-      <div class="col-12" align="center">
-        <hr>
-        WSPARCIE: <a href="https://propublicobono-rp.org/"> <u>propublicobono-rp.org</u> </a> | <a href="https://ffon.pl/"> <u>ffon.pl</u> </a>
-        <hr>
+    <div class="container pb-5 pt-5">
+      <div class="row">
+          <div class="col-4" align="center">
+            <a href="#">Redakcja</a>  |
+            <a href="#">Regulamin</a>  |
+            <a href="#">O portalu</a>  |
+            <a href="#">Zgłoś naruszenie</a>  |
+            <a href="#">Wydawca</a>
+          </div>
+          <div class="col-4" align="center">
+            ⓒ 2019 <a href="{{ route( 'index' ) }}"> <b> <u>PLPORTAL.pl</u> </b> </a> Wszelkie prawa zastrzeżone. </br>
+          </div>
+          <div class="col-4" align="center">
+            Developed by <a href="https://fundacjaglosmlodych.org/"> <u>Fundacja na Rzecz Promocji i Rozwoju „Głos Młodych”</u> </a>  and Marcin Larek
+          </div>
       </div>
     </div>
 </div>
